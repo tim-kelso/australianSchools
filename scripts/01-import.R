b45bf6bf6b3820e994a126340f0c9257a8526a06
Tim Kelso <tim.kelso@newcastle.edu.au>
2023-04-03 15:31:54 +1000

    Calculation of net change in school number by year using school_location datasets from 2019-2022

--- scripts/01-import.R
+++ scripts/01-import.R
@@ -1,9 +1,16 @@
+#QTA data_raw path
+QTAdataPath <- "C:/Users/twk208/OneDrive - The University of Newcastle/QTA/data_raw/ACARA/"
+QTAdataProcessedPath <- "C:/Users/twk208/OneDrive - The University of Newcastle/QTA/data_processed/ACARA/"
 
 #import ACARA dataset
-schoolList_profile_yearly <- read.csv("../data_raw/ACARA_school_profile_2008-2021.csv")
-schoolList_profile_2021 <- read.csv("../data_raw/ACARA_school_profile_2021.csv")
-schoolList_location_2021 <- read.csv("../data_raw/ACARA_school_location_2021.csv")
+schoolProfile_yearly <- read_excel(paste0(QTAdataPath,"ACARA_school_profile_2008-2022.xlsx"), sheet = "SchoolProfile 2008-2022", .name_repair = "universal")
+schoolLocation_2022 <- read_excel(paste0(QTAdataPath, "ACARA_school_location_2022.xlsx"), sheet = "SchoolLocations 2022", .name_repair = "universal") %>% mutate(Calendar.Year = 2022)
+schoolLocation_2021 <- read_excel(paste0(QTAdataPath,"ACARA_school_location_2021.xlsx"), sheet = "SchoolLocations 2021", .name_repair = "universal")%>% mutate(Calendar.Year = 2021)
+schoolLocation_2020 <- read_excel(paste0(QTAdataPath,"ACARA_school_location_2020.xlsx"), sheet = "SchoolLocations 2020", .name_repair = "universal")%>% mutate(Calendar.Year = 2020)
+schoolLocation_2019 <- read_excel(paste0(QTAdataPath,"ACARA_school_location_2019.xlsx"), sheet = "School Location 2019", .name_repair = "universal")%>% mutate(Calendar.Year = 2019)
+schoolProfile_2022 <- read_excel(paste0(QTAdataPath, "ACARA_school_profile_2022.xlsx"), sheet = "SchoolProfile 2022", .name_repair = "universal")
+schoolProfile_2021 <- read_excel(paste0(QTAdataPath,"ACARA_school_profile_2021.xlsx"), sheet = "SchoolProfile 2021", .name_repair = "universal")
 
 
 #import list of schools that don't have ACARAs in Dynamics
-ACARAs_NotInDynamics <- read.csv("../../QTA/data_raw/ACARAs_Not_In_Dynamics.csv")
+ACARAs_NotInDynamics <- read.csv(paste0(QTAdataProcessedPath, "ACARAs_Not_In_Dynamics.csv"))
--- scripts/02-clean.R
+++ scripts/02-clean.R
@@ -1,19 +1,37 @@
 #ACARA School list
-schoolList_profile_yearly <- schoolList_profile_yearly  %>%
+schoolProfile_yearly <- schoolProfile_yearly  %>%
+  mutate(across(.cols = c(State, Campus.Type:Rolled.Reporting.Description, Governing.Body, Year.Range), .fns = as.factor)) %>% 
+  mutate(Geolocation = factor(Geolocation, levels = c("Major Cities", "Inner Regional", "Outer Regional", "Remote", "Very Remote"), ordered = TRUE),
+         School.Sector = factor(School.Sector, levels = c("Government", "Catholic", "Independent")),
+         School.Type = factor(School.Type, levels = c("Primary", "Secondary", "Combined", "Special")))
+
+schoolLocation_yearly <- schoolLocation_2019  %>%
+  bind_rows(schoolLocation_2020, schoolLocation_2021, schoolLocation_2022) %>% 
+  mutate(across(.cols = c(State, Campus.Type:Rolled.Reporting.Description, ABS.Remoteness.Area.Name), .fns = as.factor)) %>% 
+  mutate(School.Sector = factor(School.Sector, levels = c("Government", "Catholic", "Independent")),
+         School.Type = factor(School.Type, levels = c("Primary", "Secondary", "Combined", "Special")))
+
+#ACARA School list
+schoolProfile_2021 <- schoolProfile_2021  %>%
   mutate(across(.cols = c(State, Campus.Type:Rolled.Reporting.Description, Governing.Body, Year.Range), .fns = as.factor)) %>% 
   mutate(Geolocation = factor(Geolocation, levels = c("Major Cities", "Inner Regional", "Outer Regional", "Remote", "Very Remote"), ordered = TRUE),
          School.Sector = factor(School.Sector, levels = c("Government", "Catholic", "Independent")),
          School.Type = factor(School.Type, levels = c("Primary", "Secondary", "Combined", "Special")))
 
 #ACARA School list
-schoolList_profile_2021 <- schoolList_profile_2021  %>%
+schoolLocation_2021 <- schoolLocation_2021  %>%
+  mutate(across(.cols = c(State, Campus.Type:Rolled.Reporting.Description), .fns = as.factor)) %>% 
+  mutate(School.Sector = factor(School.Sector, levels = c("Government", "Catholic", "Independent")),
+         School.Type = factor(School.Type, levels = c("Primary", "Secondary", "Combined", "Special")))
+
+schoolProfile_2022 <- schoolProfile_2022  %>%
   mutate(across(.cols = c(State, Campus.Type:Rolled.Reporting.Description, Governing.Body, Year.Range), .fns = as.factor)) %>% 
   mutate(Geolocation = factor(Geolocation, levels = c("Major Cities", "Inner Regional", "Outer Regional", "Remote", "Very Remote"), ordered = TRUE),
          School.Sector = factor(School.Sector, levels = c("Government", "Catholic", "Independent")),
          School.Type = factor(School.Type, levels = c("Primary", "Secondary", "Combined", "Special")))
 
 #ACARA School list
-schoolList_location_2021 <- schoolList_location_2021  %>%
+schoolLocation_2022 <- schoolLocation_2022  %>%
   mutate(across(.cols = c(State, Campus.Type:Rolled.Reporting.Description), .fns = as.factor)) %>% 
   mutate(School.Sector = factor(School.Sector, levels = c("Government", "Catholic", "Independent")),
          School.Type = factor(School.Type, levels = c("Primary", "Secondary", "Combined", "Special")))
